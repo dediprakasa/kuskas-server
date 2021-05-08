f013c3e0892dbd640342caaf4bf170f64005f0a3
deprak <dedprakasa@gmail.com>
2021-05-08 13:54:51 +0700

    enable async on mail

--- src/main/java/com/deltadirac/kuskas/KuskasApplication.java
+++ src/main/java/com/deltadirac/kuskas/KuskasApplication.java
@@ -3,8 +3,10 @@ package com.deltadirac.kuskas;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class KuskasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/deltadirac/kuskas/service/MailService.java
+++ src/main/java/com/deltadirac/kuskas/service/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -21,6 +22,7 @@ public class MailService {
     private final JavaMailSender mailSender;
     private final MailContentBuilder mailContentBuilder;
 
+    @Async
     public void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
