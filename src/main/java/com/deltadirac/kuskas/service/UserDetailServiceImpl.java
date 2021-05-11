c195c49905a7c67936975214a99dfc6b8cc1bb09
deprak <dedprakasa@gmail.com>
2021-05-11 12:40:49 +0700

    define userdetailservice implementation

---
+++ src/main/java/com/deltadirac/kuskas/service/UserDetailServiceImpl.java
@@ -0,0 +1,40 @@
+package com.deltadirac.kuskas.service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+import com.deltadirac.kuskas.model.User;
+import com.deltadirac.kuskas.repository.UserRepository;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional
+                .orElseThrow(() -> new UsernameNotFoundException("username " + username + " is not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), true, true, true, getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+}
